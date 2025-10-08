b4a7feaf74c00aa40f3eafedcedb284356fd782e
rhomb-real <guskovkostya.15@gmail.com>
2025-10-09 00:19:38 +0300

    Add files via upload

--- IKTpv25.py
+++ IKTpv25.py
@@ -1,28 +1,81 @@
-#Harjutus 1.1. Muutujad ja sisend
 
-
-from traceback import print_exception
-
-
+#1
 print("Tere maailm!")
 nimi=input("Sisesta oma nimi: ") #sisend ja ootab enterit
 print(f"Tere maailm! Tervitan sind {nimi}.")
 vanus=int(input("Sisesta oma vanus: ")) #int teisendab stringi
 print(f"Tere maailm! Sind {nimi}. Sa oled {vanus} aastat vana.")
 
-#2.
-#Mis tüüpi on järgnevad muutujad:
-#a)vanus = 18
-#b)eesnimi = "Jaak"
-#c)pikkus = 16.5
-#d) kas_käib_koolis = True
-#Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende
-#Kirjuta kood tüüpide kontrollimiseks
-vanus = 18
-eesnimi = "Jaak"
-pikkus = 1.65
-kas_käib_koolis = True
+#2
+vanus = 18 #int
+eesnimi = "Jaak" #string
+pikkus = 1.65 #float
+kas_kÃ¤ib_koolis = True #bool
 print(f"vanus {vanus} on: {type(vanus)}")
 print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
 print(f"pikkus {pikkus} on: {type(pikkus)}")
-print(f"kas_käib_koolis {kas_käib_koolis} on: {type(kas_käib_koolis)}")
+print(f"kas_kÃ¤ib_koolis {kas_kÃ¤ib_koolis} on: {type(kas_kÃ¤ib_koolis)}")
+
+#3
+import math
+import random
+
+
+kommid = random.randint(0, 10)
+print(f"Kommid on {kommid}.")
+sub=int(input("Mitu kommi vÃµta? "))
+kommid = kommid - sub
+print(f"Kommid on {kommid}.")
+
+#4
+circle = int(input("kui pikk on ringjoon?  "))
+diam = circle / 3.15
+print(f"Ring diameter on {diam}")
+
+#5
+N=int(input("mis on N? "))
+m=int(input("Mis on m? "))
+diag = (m * N) ** 2 + (m * m) ** 2 
+print(f"diag = {math.sqrt(diag)}")
+
+
+#6
+aeg = float(input("Mitu tundi kulus sÃµiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sÃµitsid? "))
+kiirus = aeg / teepikkus
+
+print(f"Sinu kiirus oli {kiirus} km/h") #str(kiirus)
+
+#7
+
+import statistics
+from statistics import mean
+
+d = [5, 10, 15, 20, 25]
+x = mean(d)
+sum = sum(d)
+div=int(input("div on? "))
+res = sum / div
+print(f"result on {res}, mean on {x}")
+
+#8
+
+print("""\         @..@
+         (----)
+        ( \__/ )
+        ^^ "" ^^  
+                 """)
+
+#9
+
+a=int(input("mis a on? "))
+b=int(input("mis b on? "))
+c=int(input("mis c on? "))
+P = a + b + c
+print(f"P on {P}")
+
+#10
+
+I=int(input("Mitu on inimest? "))
+Cash = (0.1 * 12.90) / I
+print(f"Raha on {Cash}")
